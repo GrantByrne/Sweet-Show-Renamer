b51f4195268529b93ae8e03bd6517b648eec1652
holymoo <grant@grantbyrne.com>
2013-11-13 07:20:35 -0500

    Added the beginnings of a service to persist the names of shows.

---
+++ SweetShowRenamer/SweetShowRenamer.Lib/Service/ShowService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetShowRenamer.Lib.Service
+{
+    public class ShowService
+    {
+        public string GetUrl(string showName)
+        {
+            return "http://epguides.com/" + showName.Replace("The ", "").Replace(" ", "") + "/";
+        }
+
+        public string GetShowNumberString(int season, int episode)
+        {
+            return "s" + season.ToString("00") + "e" + episode.ToString("00");
+        }
+    }
+}
